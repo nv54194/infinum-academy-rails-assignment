e45f4f62d12c4548510606e70a02c01d75b42167
Nikola Vlahović <nv54194@fer.hr>
2025-07-16 16:37:20 +0200

    Add specs for Resolver.city_id and fix city list path

--- app/domains/open_weather_map/resolver.rb
+++ app/domains/open_weather_map/resolver.rb
@@ -2,14 +2,14 @@ require 'json'
 
 module OpenWeatherMap
   module Resolver
-    PATH = File.expand_path('city_ids.json', __dir__)
+    PATH = File.expand_path('city.list.json', __dir__)
 
     def self.city_data
       @city_data ||= JSON.parse(File.read(PATH))
     end
 
     def self.city_id(city_name)
-      city_data.find { |c| c['name'].casecmp?(city_name) }&.fetch('id')
+      city_data.find { |c| c.fetch('name').casecmp?(city_name) }&.fetch('id')
     end
   end
 end
