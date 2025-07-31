5ca9edf7aa8124a6f37940058c86f07ed2ef51ff
Nikola Vlahović <nv54194@fer.hr>
2025-07-31 10:19:04 +0200

    remove whitespace

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
-    Bullet.raise = true 
+    Bullet.raise = true
   end
 end
