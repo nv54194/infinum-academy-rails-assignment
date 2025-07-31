2903d83684aa9150adbfad62b860151234bb8930
Nikola Vlahović <nv54194@fer.hr>
2025-07-31 11:02:07 +0200

    fix

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,7 +5,7 @@ module Api
     def create
       user = User.find_by(email: session_params[:email])
       if user&.authenticate(session_params[:password])
-        user.regenerate_token unless test_token(user)
+        user.regenerate_token
         render json: SessionSerializer.render({ token: user.reload.token, user: user },
                                               root: :session), status: :created
       else
--- app/queries/statistics/companies_query.rb
+++ app/queries/statistics/companies_query.rb
@@ -4,14 +4,20 @@ module Statistics
       @relation = relation
     end
 
-    def with_stats
+    def with_stats # rubocop:disable Metrics/MethodLength
       @relation
         .left_joins(flights: :bookings)
         .select(
           'companies.id AS company_id',
           'COALESCE(SUM(bookings.no_of_seats * bookings.seat_price), 0) AS total_revenue',
           'COALESCE(SUM(bookings.no_of_seats), 0) AS total_no_of_booked_seats',
-          'COALESCE(AVG(bookings.seat_price), 0.0) AS average_price_of_seats'
+          "CASE
+             WHEN COALESCE(SUM(bookings.no_of_seats), 0) = 0 THEN 0
+             ELSE ROUND(
+               COALESCE(SUM(bookings.no_of_seats * bookings.seat_price), 0)::numeric /
+               COALESCE(SUM(bookings.no_of_seats), 0), 2
+             )
+           END AS average_price_of_seats"
         )
         .group('companies.id')
     end
