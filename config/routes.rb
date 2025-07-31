d8a58645c6b94abf70c97f3c5225d62f503d0153
Nikola Vlahović <nv54194@fer.hr>
2025-07-31 08:20:13 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    resources :users, except: [:new, :edit]
-    resources :companies, except: [:new, :edit]
-    resources :flights, except: [:new, :edit]
-    resources :bookings, except: [:new, :edit]
-    resource :session, only: [:create, :destroy]
-  end
+ # namespace :api do
+  #  resources :users, except: [:new, :edit]
+  # resources :companies, except: [:new, :edit]
+  #  resources :flights, except: [:new, :edit]
+  #  resources :bookings, except: [:new, :edit]
+  #  resource :session, only: [:create, :destroy]
+ # end
 end
